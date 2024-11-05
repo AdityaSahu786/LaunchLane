69fca70f63b2926d166e864844517eedded3262d
AdityaSahu786 <sahuadi786183@gmail.com>
2024-11-06 00:53:47 +0530

    Minor Update

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
@@ -7,6 +7,8 @@ import Link from "next/link";
 import Image from "next/image";
 
 import markdownit from 'markdown-it'
+import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
 
 const md = markdownit();
 
@@ -60,7 +62,22 @@ const Page = async ({ params}: { params: Promise<{id: string}>}) => {
                <h3 className="text-30-bold">
                 Pitch Details
                </h3>
+               {parsedContent ? (
+                <article 
+                   className="prose max-w-4xl font-work-sans break-all "
+                  dangerouslySetInnerHTML={{ __html: parsedContent }}
+                  />
+               ): (
+                 <p className="no-result">No details provided</p>
+               )}
             </div>
+
+            <hr className="divider" />
+
+            {/* TODO: EDITOR SELECTED STARTUPS */}
+            <Suspense fallback={<Skeleton className="view_skeleton" />}>
+               <View id={id} />
+            </Suspense>
       </section>
       
     </>
